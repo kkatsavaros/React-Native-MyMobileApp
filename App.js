e24b9c4b913eecd329f8e0d8b8ddc4879ebc2ebc
kkatsavar <kkatsavar@c-ts.gr>
2025-02-15 15:42:24 +0200

    My second commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainNavigation from './navigation/MainNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MainNavigation/>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
---
+++ screens/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import Header from '../../components/Header/Header';
+
+import style from './style';
+import globalStyle from '../../assets/styles/globalStyle';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <Header title={'Home page'} type={1} />
+      <Header title={'Home page'} type={2} />
+      <Header title={'Home page'} type={3} />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
