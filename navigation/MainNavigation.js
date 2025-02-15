c75afc917c824d741de949ab10c44d566da3b180
kkatsavar <kkatsavar@c-ts.gr>
2025-02-15 20:07:47 +0200

    Added Drawer Navigator

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,17 +1,31 @@
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+//
 import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
+//
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
+// Drawer
+const MainMenuNavigation = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen name={Routes.Profile} component={Profile} />
+    </Drawer.Navigator>
+  );
+};
+
+// Stack
 const MainNavigation = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{header: () => null, headerShown: false}}
-      initialRouteName={Routes.Home}>
-      <Stack.Screen name={Routes.Home} component={Home} />
-      <Stack.Screen name={Routes.Profile} component={Profile} />
+    <Stack.Navigator screenOptions={{header: () => null, headerShown: false}}>
+      {/* <Stack.Screen name={Routes.Home} component={Home} />       
+      <Stack.Screen name={Routes.Profile} component={Profile} /> */}
+      <Stack.Screen name={'Drawer'} component={MainMenuNavigation} />
     </Stack.Navigator>
   );
 };
--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
+import Header from '../../components/Header/Header';
 
 const Profile = ({navigation}) => {
   return (
@@ -8,6 +9,7 @@ const Profile = ({navigation}) => {
       <TouchableOpacity onPress={() => navigation.goBack()}>
         <Text style={{color: 'blue'}}>Go Back</Text>
       </TouchableOpacity>
+      <Header title={'Profile page'} type={1} />
       <Text>Welcome to profile page</Text>
     </SafeAreaView>
   );
