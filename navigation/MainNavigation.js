c460cbb761cb4b4b6d99894d401e89861b6a75b5
kkatsavar <kkatsavar@c-ts.gr>
2025-02-15 18:42:44 +0200

    Added Second Screen

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,13 +1,17 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
+import Profile from '../screens/Profile/Profile';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
   return (
-    <Stack.Navigator screenOptions={{header: () => null, headerShown: false}}>
+    <Stack.Navigator
+      screenOptions={{header: () => null, headerShown: false}}
+      initialRouteName={Routes.Home}>
       <Stack.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen name={Routes.Profile} component={Profile} />
     </Stack.Navigator>
   );
 };
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text, View, TouchableOpacity} from 'react-native';
 import Header from '../../components/Header/Header';
+import {Routes} from '../../navigation/Routes';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Home page - ddd'} type={1} />
       <Header title={'Home page'} type={2} />
       <Header title={'Home page'} type={3} />
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate(Routes.Profile);
+        }}>
+        <View>
+          <Text>Go to Profile Page</Text>
+        </View>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
